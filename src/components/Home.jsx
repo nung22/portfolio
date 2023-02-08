deea7f384c362f24749cb85ea8d3aaf26ab3e28b
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-08 08:50:58 -0800

    Add animations to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import { Paper } from '@mui/material';
+import './Home.css'
 
 export default function Home() {
+
   return (
-    <div className='flex justify-center mt-72'>
-      <Paper className='flex flex-col gap-8 p-16' style={{borderRadius:".9rem"}} elevation={10}>
-        <p className='text-xl font-semibold text-indigo-400'>Hello, my name is</p>
-        <p className='text-7xl font-bold text-blue-500'>Nicholas Ung.</p>
-        <p className='text-6xl font-bold text-gray-400'>I'm a software engineer &<br/>full stack web developer.</p>
-        <p className='text-2xl text-indigo-400'>My passion lies in solving business problems through<br/>
-        human-centered design. I specialize in building (and<br/>
-        periodically planning) excellent digital experiences.</p>
-      </Paper>
+    <div>
+      <div className='flex justify-center mt-20 z-100'>
+        <div className='flex flex-col gap-8 p-16 z-30' style={{borderRadius:".9rem"}} elevation={10}>
+          <p className='text-xl font-semibold'>Hello, my name is</p>
+          <p className='text-7xl font-bold'>Nicholas Ung.</p>
+          <p className='text-6xl font-bold'>I'm a web developer.</p>
+          <p className='text-2xl'>My passion lies in solving business problems through<br/>
+          human-centered design. I specialize in building (and<br/>
+          periodically planning) excellent digital experiences.</p>
+        </div>
+      </div>
+      <div className="flex justify-center opacity-30 -translate-y-96 z-0">
+        <div id="left-box" className="flex items-center rotate-90 rounded-full w-36 h-36 z-20 
+        bg-gradient-to-r 
+        from-blue-400 
+        to-orange-500 
+        via-purple-500
+        animate-gradient-x
+        "></div>
+        <div id="middle-box" className="flex items-center rotate-90 rounded-full w-80 h-80 z-10
+        bg-gradient-to-r 
+        from-pink-500 
+        to-yellow-500 
+        via-green-400
+        animate-gradient-x
+        " style={{}}></div>
+        <div id="right-box" className="flex items-center rotate-90 rounded-full w-48 h-48 mt-24 z-0
+        bg-gradient-to-r 
+        from-blue-400 
+        to-orange-500 
+        via-purple-500
+        animate-gradient-x
+        "></div>
+
+      </div>
     </div>
   );
 }
