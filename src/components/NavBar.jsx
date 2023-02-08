3e8b88530156bd4294ed408dbb6964a1ae13692c
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-08 08:51:01 -0800

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import resume from '../assets/Nicholas_Ung_Resume.02.2023.pdf'
 
 export default function NavBar() {
   return (
     <Stack direction="row" spacing={3}>
-      <Button size="large" style={{fontWeight:"600", fontSize:"1rem"}} href="#text-buttons">About</Button>
-      <Button size="large" style={{fontWeight:"600", fontSize:"1rem"}} href="#text-buttons">Experience</Button>
-      <Button size="large" style={{fontWeight:"600", fontSize:"1rem"}} href="#text-buttons">Work</Button>
-      <Button size="large" style={{fontWeight:"600", fontSize:"1rem"}} href="#text-buttons">Contact</Button>
-      <Button size="medium" variant="outlined" color="success" 
+      <Link to={"/about"}><Button size="large" style={{fontWeight:"600", fontSize:"1rem"}}>About</Button></Link>
+      <Link to={"/work"}><Button size="large" style={{fontWeight:"600", fontSize:"1rem"}}>Work</Button></Link>
+      <Link to={"/contact"}><Button size="large" style={{fontWeight:"600", fontSize:"1rem"}}>Contact</Button></Link>
+      <Button size="medium" variant="outlined" color="success" href={resume}
       style={{fontWeight:"600", fontSize:"1rem", borderWidth:".13rem", borderRadius: "1.5rem"}}>Resume</Button>
     </Stack>
   );
