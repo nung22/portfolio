09c5842545e5f51a64aa7bb09526a74bd2c0aa2b
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-08 08:51:22 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Route, Routes, Navigate } from 'react-router-dom'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { IconButton } from '@mui/material';
 import logo from './assets/logo.png'
+import Favicon from "react-favicon";
 import ThemeSwitch from './components/ThemeSwitch';
 import NavBar from './components/NavBar'
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   const [colorTheme, setColorTheme] = useState('dark');
@@ -41,6 +43,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <Favicon url={logo}></Favicon>
       <CssBaseline />
       <div className='p-5 px-12 flex-col'>
         <div className='flex justify-between items-center'>
@@ -52,7 +55,11 @@ function App() {
             <ThemeSwitch onClick={onToggle}/>
           </div>
         </div>
-        <Home/>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home"/>}/>
+          <Route element={<Home/>} path="/home" />
+          <Route element={<About/>} path="/about" />
+        </Routes>
       </div>
     </ThemeProvider>
   );
