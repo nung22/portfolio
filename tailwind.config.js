fe7b5055b2a231174f2c17bc63ef72ef08dfb8e0
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-08 08:51:29 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "gradient-x": "gradient-x 8s ease infinite",
+        "gradient-y": "gradient-y 8s ease infinite",
+        "gradient-xy": "gradient-xy 8s ease infinite",
+        "wiggle": 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        'wiggle': {
+          '0%, 100%': {
+              transform: 'rotate(-3deg)'
+          },
+          '50%': {
+              transform: 'rotate(3deg)'
+          },
+        },
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center",
+          },
+        },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
