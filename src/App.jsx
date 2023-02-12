23a4fc57a1e5703930762195f056a1f306e4fd4b
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-11 17:53:53 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import React, { useState } from 'react';
 import { useNavigate, Route, Routes, Navigate } from 'react-router-dom'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -62,15 +64,22 @@ function App() {
         </Routes>
       </div>
       <div className='flex justify-center'>
-        <div className='absolute bottom-0 left-16'>
-          <h2>nung@ucla.edu</h2>
-          <div className='w-px h-32 bg-white'></div>
+        <div className='absolute bottom-32 left-0 flex items-center rotate-90 gap-7' style={{marginLeft:"-4rem", marginBottom:"-2.2rem"}}>
+          <a href="https://www.linkedin.com/in/nicholas-ung-285495123/" className='hover:text-blue-500' 
+          style={{ paddingBottom:".2rem"}} target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon className="-rotate-90"/>
+          </a>
+          <a href="https://github.com/nung22" className='hover:text-blue-500' 
+          style={{ paddingBottom:".2rem"}} target="_blank" rel="noopener noreferrer">
+            <GitHubIcon className="-rotate-90"/>
+          </a>
+          <div className={`h-px ${colorTheme === 'dark' ? 'bg-white' : 'bg-black'}`} style={{width:"7rem"}}></div>
         </div>
         <a href="https://github.com/nung22/portfolio" target="_blank" rel="noopener noreferrer" 
-        id="important-text" className='text-sm font-semibold opacity-60 text-center absolute bottom-8'>Designed & Built by Nicholas Ung</a>
-        <div className='absolute bottom-32 right-0 flex items-center rotate-90 gap-7' style={{marginRight:"-3rem", marginBottom:"-.9rem"}}>
-          <h2 className='hover:-translate-x-2 hover:text-blue-500'>nung@ucla.edu</h2>
-          <div className='h-px bg-white' style={{width:"7rem"}}></div>
+        className='text-sm font-semibold opacity-60 text-center absolute bottom-8 hover:text-blue-500 hover:opacity-100'>Designed & Built by Nicholas Ung</a>
+        <div className='absolute bottom-32 right-0 flex items-center rotate-90 gap-7' style={{marginRight:"-5.5rem", marginBottom:"-.6rem"}}>
+          <a href="mailto:nung@ucla.edu" className='hover:text-blue-500 font-semibold text-lg' style={{ paddingBottom:".2rem"}}>nung@ucla.edu</a>
+          <div className={`h-px ${colorTheme === 'dark' ? 'bg-white' : 'bg-black'}`} style={{width:"7rem"}}></div>
         </div>
       </div>
     </ThemeProvider>
