61b4faa89ca260f00166ffa95ece1fcae10e493d
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-17 11:07:17 -0800

    Update formatting for home page
    Fix links to email, Github, and LinkedIn to bottom of page. Set link to portfolio site Github repo at bottom of page.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { IconButton } from '@mui/material';
 import logo from './assets/logos/logo.png'
-import Favicon from "react-favicon";
 import ThemeSwitch from './components/ThemeSwitch';
 import NavBar from './components/NavBar'
 import Home from './components/Home';
@@ -44,45 +43,46 @@ function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Favicon url={logo}></Favicon>
-      <CssBaseline />
-      <div className='flex justify-between items-center px-2 py-2 sm:py-2 sm:px-8 opacity-80 scroll-invisible'>
-          <IconButton aria-label="logo" onClick={() => navigate('/')}>
-            <img className='w-8 sm:w-14' src={logo} alt="logo" />
-          </IconButton>
-        <div className='flex items-center gap-5'>
-          <NavBar />
-          <ThemeSwitch onClick={onToggle}/>
+    <div className="flex flex-col h-screen w-full">
+      <ThemeProvider theme={theme} className="w-full">
+        <CssBaseline />
+        <div className='flex justify-between items-center px-2 py-2 sm:py-2 sm:px-8 opacity-80 scroll-invisible'>
+            <IconButton aria-label="logo" onClick={() => navigate('/')}>
+              <img className='w-8 sm:w-14' src={logo} alt="logo" />
+            </IconButton>
+          <div className='flex items-center gap-5'>
+            <NavBar/>
+            <ThemeSwitch onClick={onToggle}/>
+          </div>
         </div>
-      </div>
-      <div className='p-5 flex flex-col'>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home"/>}/>
-          <Route element={<Home/>} path="/home" />
-          <Route element={<About/>} path="/about" />
-        </Routes>
-      </div>
-      <div className='flex justify-center'>
-        <div className='absolute bottom-32 left-0 flex items-center rotate-90 gap-7' style={{marginLeft:"-4rem", marginBottom:"-2.2rem"}}>
-          <a href="https://www.linkedin.com/in/nicholas-ung-285495123/" className='hover:text-blue-500' 
-          style={{ paddingBottom:".2rem"}} target="_blank" rel="noopener noreferrer">
-          <LinkedInIcon className="-rotate-90"/>
-          </a>
-          <a href="https://github.com/nung22" className='hover:text-blue-500' 
-          style={{ paddingBottom:".2rem"}} target="_blank" rel="noopener noreferrer">
-            <GitHubIcon className="-rotate-90"/>
-          </a>
-          <div className={`h-px ${colorTheme === 'dark' ? 'bg-white' : 'bg-black'}`} style={{width:"7rem"}}></div>
+        <div className='p-5 flex flex-col'>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home"/>}/>
+            <Route element={<Home/>} path="/home" />
+            <Route element={<About/>} path="/about" />
+          </Routes>
         </div>
-        <a href="https://github.com/nung22/portfolio" target="_blank" rel="noopener noreferrer" 
-        className='text-sm font-semibold opacity-60 text-center absolute bottom-8 hover:text-blue-500 hover:opacity-100'>Designed & Built by Nicholas Ung</a>
-        <div className='absolute bottom-32 right-0 flex items-center rotate-90 gap-7' style={{marginRight:"-5.5rem", marginBottom:"-.6rem"}}>
+        <div className='fixed bottom-20 left-0 flex items-center rotate-90 gap-5' style={{marginLeft:"-2.8rem"}}>
+          <a href="https://www.linkedin.com/in/nicholas-ung-285495123/" className='hover:text-blue-500'
+          style={{ paddingBottom:".2rem"}} target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon className='-rotate-90'/>
+          </a>
+          <a href="https://github.com/nung22" className='hover:text-blue-500'
+          style={{ paddingBottom:".2rem"}} target="_blank" rel="noopener noreferrer">
+            <GitHubIcon className='-rotate-90'/>
+          </a>
+          <div className={`h-px ${colorTheme === 'dark' ? 'bg-white' : 'bg-black'}`} style={{width:"8rem"}}></div>
+        </div>
+        <div className='fixed bottom-28 right-0 flex items-center rotate-90 gap-7' style={{marginRight:"-5rem"}}>
           <a href="mailto:nung@ucla.edu" className='hover:text-blue-500 font-semibold text-lg' style={{ paddingBottom:".2rem"}}>nung@ucla.edu</a>
           <div className={`h-px ${colorTheme === 'dark' ? 'bg-white' : 'bg-black'}`} style={{width:"7rem"}}></div>
         </div>
+      </ThemeProvider>
+      <div className='mt-auto w-full flex justify-center pb-4'>
+        <a href="https://github.com/nung22/portfolio" target="_blank" rel="noopener noreferrer"
+          className='text-sm font-semibold opacity-60 text-center hover:text-blue-500 hover:opacity-100'>Designed & Built by Nicholas Ung</a>
       </div>
-    </ThemeProvider>
+    </div>
   );
 }
 
