bd1077aed081f99fdc42fb5c167675ff30224ee4
Nicholas Ung <nicholasung22@gmail.com>
2026-09-10 14:21:35 -0700

    Remove BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "@fontsource/fira-code";
 import "@fontsource/fira-code/700.css";
 import "./index.css";
-import App from './App.tsx';
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
